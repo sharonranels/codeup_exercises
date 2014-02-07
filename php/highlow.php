e9e93373b6a05045dd75dc829dd6a4d00d9c3623
sharonranels <sharonranels@yahoo.com>
2014-02-07 11:52:49 -0600

    Game complete

--- php/highlow.php
+++ php/highlow.php
@@ -14,20 +14,27 @@ $user_guess = fgets(STDIN);
 echo($user_guess);
 
 //if user's guess is high, output LOWER
+while($user_guess != $rndnumber) {
+
 if ($user_guess > $rndnumber) {
 	fwrite(STDOUT, "LOWER\n");
 	fwrite(STDOUT, 'Guess again ');
-	$user_guess - fgets(STDIN);
-
-//if user's guess is low, output HIGHER
-	//elseif ($user_guess < $rndnumber) {
-	//fwrite(STDOUT, "HIGHER\n");
-	//fwrite(STDOUT, 'Guess again ');
-	//$user_guess - fgets(STDIN);
-//}
+	$user_guess = fgets(STDIN);
 }
+
+//if user's guess is low, output HIGHER  
+	elseif ($user_guess < $rndnumber) {
+	fwrite(STDOUT, "HIGHER\n");
+	fwrite(STDOUT, 'Guess again ');
+	$user_guess = fgets(STDIN);
+}
+
+
+}
+ echo "GOOD GUESS\n";
+
 
 //if user's guess is correct, declare GOOD GUESS
 
 //Exit
-
+?>
