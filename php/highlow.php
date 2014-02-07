5acb1fcffebc3110aba3456660e16a08338cdae6
sharonranels <sharonranels@yahoo.com>
2014-02-07 12:13:08 -0600

    Number of guesses added

--- php/highlow.php
+++ php/highlow.php
@@ -2,6 +2,7 @@
 
 //pick random number
 $rndnumber = mt_rand(1 , 100);
+$count_guesses = 0;
 echo($rndnumber);
 
 //prompt user to guess number
@@ -11,6 +12,7 @@ fwrite(STDOUT, 'Guess number ');
 //Get input from user
 
 $user_guess = fgets(STDIN);
+$count_guesses += 1;
 echo($user_guess);
 
 //if user's guess is high, output LOWER
@@ -20,6 +22,7 @@ if ($user_guess > $rndnumber) {
 	fwrite(STDOUT, "LOWER\n");
 	fwrite(STDOUT, 'Guess again ');
 	$user_guess = fgets(STDIN);
+	$count_guesses += 1;
 }
 
 //if user's guess is low, output HIGHER  
@@ -27,11 +30,13 @@ if ($user_guess > $rndnumber) {
 	fwrite(STDOUT, "HIGHER\n");
 	fwrite(STDOUT, 'Guess again ');
 	$user_guess = fgets(STDIN);
+	$count_guesses += 1;
 }
 
 
 }
  echo "GOOD GUESS\n";
+ echo($count_guesses) . " Guesses\n";
 
 
 //if user's guess is correct, declare GOOD GUESS
