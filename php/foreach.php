f96946dea9826b0a6c471874c4f4545633c39b95
sharonranels <sharonranels@yahoo.com>
2014-02-10 11:27:04 -0600

    final loop

--- php/foreach.php
+++ php/foreach.php
@@ -20,23 +20,35 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 // 		echo "{$thing} is a string\n";
 // 	}
 // }
-//foreach ($data as $datum) {
-//if (is_numeric($datum))
- //       echo "{$datum} is a number\n";
-//    } else if (is_string($datum)) {
-//        echo "{$datum} is a string\n";
-//    }
-//}
 
+
+
+// foreach ($things as $thing) {
+// 	if (is_scalar($thing)) {
+// 		echo "{$thing} is a scalar\n";
+	
+// }
+// }
 
 
 
 foreach ($things as $thing) {
-	if (is_scalar($thing)) {
-		echo "{$thing} is a scalar\n";
-	
+	if (is_int($thing)) {
+		echo "$thing\n";
+	} elseif (is_float($thing)) {
+		echo "$thing\n";
+	}elseif (is_bool($thing)) {
+		echo "$thing\n";
+	}elseif (is_array($thing)) {
+	foreach ($thing as $arraything) {
+ 			echo "$arraything\n";
+ 	 		}
+		//echo "{$thing} is a array\n"; - substituted by above
+	}elseif (is_null($thing)) {
+		echo "$thing\n";
+		}else {
+		echo "$thing\n";
+	}
 }
-}
-
 
 ?>
