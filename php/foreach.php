0a6c95b290f31a5ba3733cc2ea85df2608c8a4ff
sharonranels <sharonranels@yahoo.com>
2014-02-10 11:14:30 -0600

    first loop in exercise

---
+++ php/foreach.php
@@ -0,0 +1,33 @@
+<?php
+
+$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+
+foreach ($things as $thing) {
+	if (is_int($thing)) {
+		echo "{$thing} is an integer\n";
+	} elseif (is_float($thing)) {
+		echo "{$thing} is a float\n";
+	}elseif (is_bool($thing)) {
+		echo "{$thing} is a boolean\n";
+	}elseif (is_array($thing)) {
+		foreach ($thing as $arraything) {
+			echo "{$arraything} is an array item\n";
+		}
+		//echo "{$thing} is a array\n"; - substituted by above
+	}elseif (is_null($thing)) {
+		echo "{$thing} is a null\n";
+		}else {
+		echo "{$thing} is a string\n";
+	}
+}
+//foreach ($data as $datum) {
+//if (is_numeric($datum))
+ //       echo "{$datum} is a number\n";
+//    } else if (is_string($datum)) {
+//        echo "{$datum} is a string\n";
+//    }
+//}
+
+
+
+?>
