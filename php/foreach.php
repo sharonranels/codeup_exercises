42bb3de3c5788d301195b7b4668ae831d9b79ecd
sharonranels <sharonranels@yahoo.com>
2014-02-10 11:20:04 -0600

    loop 2 in exercise - check if scalar

--- php/foreach.php
+++ php/foreach.php
@@ -2,24 +2,24 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
-foreach ($things as $thing) {
-	if (is_int($thing)) {
-		echo "{$thing} is an integer\n";
-	} elseif (is_float($thing)) {
-		echo "{$thing} is a float\n";
-	}elseif (is_bool($thing)) {
-		echo "{$thing} is a boolean\n";
-	}elseif (is_array($thing)) {
-		foreach ($thing as $arraything) {
-			echo "{$arraything} is an array item\n";
-		}
-		//echo "{$thing} is a array\n"; - substituted by above
-	}elseif (is_null($thing)) {
-		echo "{$thing} is a null\n";
-		}else {
-		echo "{$thing} is a string\n";
-	}
-}
+// foreach ($things as $thing) {
+// 	if (is_int($thing)) {
+// 		echo "{$thing} is an integer\n";
+// 	} elseif (is_float($thing)) {
+// 		echo "{$thing} is a float\n";
+// 	}elseif (is_bool($thing)) {
+// 		echo "{$thing} is a boolean\n";
+// 	}elseif (is_array($thing)) {
+// 		foreach ($thing as $arraything) {
+// 			echo "{$arraything} is an array item\n";
+// 		}
+// 		//echo "{$thing} is a array\n"; - substituted by above
+// 	}elseif (is_null($thing)) {
+// 		echo "{$thing} is a null\n";
+// 		}else {
+// 		echo "{$thing} is a string\n";
+// 	}
+// }
 //foreach ($data as $datum) {
 //if (is_numeric($datum))
  //       echo "{$datum} is a number\n";
@@ -28,6 +28,15 @@ foreach ($things as $thing) {
 //    }
 //}
 
+
+
+
+foreach ($things as $thing) {
+	if (is_scalar($thing)) {
+		echo "{$thing} is a scalar\n";
+	
+}
+}
 
 
 ?>
