1a467ebcd915ee1f94ed2d39613dfc182eae22ad
sharonranels <sharonranels@yahoo.com>
2014-02-10 14:46:09 -0600

    loop break after 10

--- php/break_continue.php
+++ php/break_continue.php
@@ -1,9 +1,18 @@
 <?php
 
+// for ($i = 1; $i <= 100; $i++) {
+//     if ($i % 2 == 0) {
+//        	echo $i . "\n";
+//         continue;
+    
+// }
+// }
+
+
 for ($i = 1; $i <= 100; $i++) {
-    if ($i % 2 == 0) {
        	echo $i . "\n";
-        continue;
+    if ($i >= 10) {
+        break;
     
 }
 }
