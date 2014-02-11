a96973e4a4f217b663f5b19704fb708fffa4c049
sharonranels <sharonranels@yahoo.com>
2014-02-11 14:39:25 -0600

    fixed with master validation

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -1,61 +1,64 @@
 <?php
 
-function add($a, $b = 0) {
-	if (is_numeric($a) && is_numeric($b)) {
-    echo $a + $b . "\n";	
-	} else { 
+
+function validate($a, $b) {
+	if (!is_numeric($a) || !is_numeric($b)) {
 		echo "ERROR - you must give two numbers\n";
-	}
-    
+		var_dump($a);
+		var_dump($b);
+		exit(0);
 }
-add ("Sharon", 4);
+}
 
-
-
+function add($a, $b = 0) {
+	validate($a, $b);
+    echo $a + $b . "\n";	    
+}
+add (5, 4);
 
 
 function subtract($a, $b = 0) {
-    echo $a - $b . "\n";
+    validate($a, $b);
+    echo $a - $b. "\n";	  
 }
 subtract (5, 4);
    
 
 function multiply($a, $b = 0) {
+    validate($a, $b);
     echo $a * $b . "\n";
 }
 multiply (5, 4);
    
 
 
-function divide($a, $b = 0) {
-	
-    if (!is_numeric($a) || !is_numeric($b)) {
-		echo "ERROR - you must give two numbers\n";
-	} elseif ($b == 0) {
+
+
+function validate0($a, $b) {
+	if ($b == 0) {
 		echo "ERROR - you cannot divide by 0\n";
-		} else {
-    	echo $a / $b . "\n";
-    }
-	} 
-	
-	divide (5, 0);
+		var_dump($a);
+		var_dump($b);
+		exit(0);
+}
+}
+
+function divide($a, $b = 0) {
+	validate($a, $b);
+	validate0($a, $b);
+    echo $a / $b . "\n";
+}
+	divide (5, 4);
 
 
 
 
 function modulus($a, $b = 0) {
-    if (!is_numeric($a) || !is_numeric($b)) {
-		echo "ERROR - you must give two numbers\n";
-	} elseif ($b == 0) {
-		echo "ERROR - you cannot divide by 0\n";
-		} else {
-    	echo $a % $b . "\n";
-    }
-	} 
-	
-	modulus (5, 0);
-
-
+    validate($a, $b);
+	validate0($a, $b);
+    echo $a % $b . "\n";
+}
+	modulus (5, 4);
       
 
 ?>
