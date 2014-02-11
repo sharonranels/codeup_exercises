c386acd4a5aa40e2dcaacbe42c5a63f66f31390b
sharonranels <sharonranels@yahoo.com>
2014-02-11 10:12:32 -0600

    added reset array values/numbers

---
+++ codeup_todo_list/todo.php
@@ -0,0 +1,45 @@
+<?php
+
+// Create array to hold list of todo items
+$items = array();
+// The loop!
+do {
+    // Iterate through list items
+    foreach ($items as $key => $item) {
+        $key = $key + 1;
+        // Display each item and a newline
+        echo "[{$key}] {$item}\n";
+    }
+
+    // Show the menu options
+    echo '(N)ew item, (R)emove item, (Q)uit : ';
+
+    // Get the input from user
+    // Use trim() to remove whitespace and newlines
+    $input = strtoupper(trim(fgets(STDIN)));
+
+    // Check for actionable input
+    if ($input == 'N') {
+        // Ask for entry
+        echo 'Enter item: ';
+        // Add entry to list array
+        $items[] = trim(fgets(STDIN));
+    } elseif ($input == 'R') {
+        // Remove which item?
+        echo 'Enter item number to remove: ';
+        // Get array key
+        $rkey = trim(fgets(STDIN));
+        $rkey = $key - 1;
+        // Remove from array
+        unset($items[$rkey]);
+        $items = array_values($items);
+    }
+// Exit when input is (Q)uit
+} while ($input != 'Q');
+
+// Say Goodbye!
+echo "Goodbye!\n";
+
+// Exit with 0 errors
+exit(0);
+?>
