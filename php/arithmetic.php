42d9d23f3d0efb99c0db2b3e65c7a4321cd95aa1
sharonranels <sharonranels@yahoo.com>
2014-02-11 11:12:45 -0600

    modulus added

---
+++ php/arithmetic.php
@@ -0,0 +1,32 @@
+<?php
+
+function add($a, $b) {
+    echo $a + $b . "\n";
+    
+}
+add (5, 4);
+
+function subtract($a, $b) {
+    echo $a - $b . "\n";
+}
+subtract (5, 4);
+   
+
+function multiply($a, $b) {
+    echo $a * $b . "\n";
+}
+multiply (5, 4);
+   
+
+function divide($a, $b) {
+    echo $a / $b . "\n";
+}
+divide (5, 4);
+
+function modulus($a, $b) {
+    echo $a % $b . "\n";
+}
+modulus (5, 4);
+      
+
+?>
