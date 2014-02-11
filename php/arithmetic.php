2ba52a7c0d0f464f0b8a59fd3334eb166d415a1b
sharonranels <sharonranels@yahoo.com>
2014-02-11 13:32:39 -0600

    final

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -10,6 +10,10 @@ function add($a, $b = 0) {
 }
 add ("Sharon", 4);
 
+
+
+
+
 function subtract($a, $b = 0) {
     echo $a - $b . "\n";
 }
@@ -22,15 +26,35 @@ function multiply($a, $b = 0) {
 multiply (5, 4);
    
 
+
 function divide($a, $b = 0) {
-    echo $a / $b . "\n";
-}
-divide (5, 4);
+	
+    if (!is_numeric($a) || !is_numeric($b)) {
+		echo "ERROR - you must give two numbers\n";
+	} elseif ($b == 0) {
+		echo "ERROR - you cannot divide by 0\n";
+		} else {
+    	echo $a / $b . "\n";
+    }
+	} 
+	
+	divide (5, 0);
+
+
+
 
 function modulus($a, $b = 0) {
-    echo $a % $b . "\n";
-}
-modulus (5, 4);
+    if (!is_numeric($a) || !is_numeric($b)) {
+		echo "ERROR - you must give two numbers\n";
+	} elseif ($b == 0) {
+		echo "ERROR - you cannot divide by 0\n";
+		} else {
+    	echo $a % $b . "\n";
+    }
+	} 
+	
+	modulus (5, 0);
+
 
       
 
