34dd342b68e50e2c72c31eb4fe9a14df6971a385
sharonranels <sharonranels@yahoo.com>
2014-02-12 11:30:32 -0600

    List array items formatted for CLI

--- codeup_todo_list/todo.php
+++ codeup_todo_list/todo.php
@@ -2,15 +2,37 @@
 
 // Create array to hold list of todo items
 $items = array();
-// The loop!
-do {
-    // Iterate through list items
-    foreach ($items as $key => $item) {
+
+// List array items formatted for CLI
+function list_items($list) {
+    $todos = "";
+    foreach ($list as $key => $item) {
         $key = $key + 1;
         // Display each item and a newline
-        echo "[{$key}] {$item}\n";
+        $todos .= "[{$key}] {$item}\n";
     }
+    return $todos;
+    
 
+    // Return string of list items separated by newlines.
+    // Should be listed [KEY] Value like this:
+    // [1] TODO item 1
+    // [2] TODO item 2 - blah
+    // DO NOT USE ECHO, USE RETURN
+}
+
+// Get STDIN, strip whitespace and newlines, 
+// and convert to uppercase if $upper is true
+function get_input($upper = FALSE) 
+{
+
+    // Return filtered STDIN input
+}
+
+// The loop!
+do {
+    // Echo the list produced by the function
+    echo list_items($items);
     // Show the menu options
     echo '(N)ew item, (R)emove item, (Q)uit : ';
 
@@ -29,7 +51,7 @@ do {
         echo 'Enter item number to remove: ';
         // Get array key
         $rkey = trim(fgets(STDIN));
-        $rkey = $key - 1;
+        $rkey = $rkey - 1;
         // Remove from array
         unset($items[$rkey]);
         $items = array_values($items);
