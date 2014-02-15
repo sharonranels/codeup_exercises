aa923cae667c4d29b951656bbd6b413a29bfd847
sharonranels <sharonranels@yahoo.com>
2014-02-15 14:56:04 -0600

    highlow final

--- php/highlow.php
+++ php/highlow.php
@@ -12,8 +12,13 @@ if ($argc != 3) {
 }
 
 
-$min = is_numberic($argv[1]) ? intval($argv([1]) : 1;
-$max = is_numberic($argv[2]) ? intval($argv([1]) : 100;
+// $min = is_numberic($argv[1]) ? intval($argv([1]) : 1;
+if (is_numeric($argv[1])) {
+	$min = intval($argv[1]);
+} else {
+	$min = 1;
+}
+$max = is_numeric($argv[2]) ? intval($argv[2]) : 100;
 
 
 
@@ -22,7 +27,7 @@ if (($argc != 3) || (!is_numeric($min)) || (!is_numeric($max)) || ($min < 0) || 
 	echo "Please give two numbers between 1 and 100\n";
 	exit(0);
 } else {
-	echo "You've selected $min as a minimum and $max as a maximum.ß	
+	echo "You've selected $min as a minimum and $max as a maximum.
 Please guess the randomly seleted number in that range.\n";
 }
 
@@ -47,28 +52,27 @@ $count_guesses += 1;
 //if user's guess is high, output LOWER
 while($user_guess != $rndnumber) {
 
-if ($user_guess > $rndnumber) {
-	fwrite(STDOUT, "LOWER\n");
-	fwrite(STDOUT, 'Guess again ');
-	
-}
+	if ($user_guess > $rndnumber) {
+		fwrite(STDOUT, "LOWER\n");
+		fwrite(STDOUT, 'Guess again ');
+		
+	}
 
-//if user's guess is low, output HIGHER  
+	//if user's guess is low, output HIGHER  
 	elseif ($user_guess < $rndnumber) {
-	fwrite(STDOUT, "HIGHER\n");
-	fwrite(STDOUT, 'Guess again ');
-	
-}
-		$user_guess = fgets(STDIN);
+		fwrite(STDOUT, "HIGHER\n");
+		fwrite(STDOUT, 'Guess again ');
+		
+	}
+	$user_guess = fgets(STDIN);
 	$count_guesses += 1;
 
 }
- //if user's guess is correct, declare GOOD GUESS
- echo "GOOD GUESS\n";
- echo($count_guesses) . " Guesses\n";
-
-exit(0);
-
+//if user's guess is correct, declare GOOD GUESS
+echo "GOOD GUESS\n";
+echo($count_guesses) . " Guesses\n";
 
 //Exit
+exit(0);
+
 ?>
