2396ef09d20efa07841c28c162b1507f57d2b9c8
sharonranels <sharonranels@yahoo.com>
2014-02-15 15:39:14 -0600

    functions added

--- php/for.php
+++ php/for.php
@@ -1,23 +1,38 @@
 <?php
 
-// Write the output
-// Notice the space after the ?
 
-//fwrite(STDOUT, 'What is your first name? ');
+function get_num ($prompt_user) {
+	do {
+		fwrite(STDOUT, $prompt_user);
+		$input_num = trim(fgets(STDIN));
+		if (!is_numeric($input_num)) {
+			echo "You must give a number. Please try again.\n";
+		}
+	} while (!is_numeric($input_num));
+	return $input_num;
+}
 
-// Get the input from user
-//$first_name = fgets(STDIN);
+$start_num = get_num('Please give start number: ');
 
-// Output the user's name
-//fwrite(STDOUT, "Hello $first_name\n");
+//BEFORE FUNCTION:
 
-fwrite(STDOUT, 'Please give start number: ');
-$start_num = trim(fgets(STDIN));
+// do {
+// 	fwrite(STDOUT, 'Please give start number: ');
+// 	$start_num = trim(fgets(STDIN));
+// 	if (!is_numeric($start_num)) {
+// 		echo "You must give a number. Please try again.\n";
+// 	}
+// } while (!is_numeric($start_num));
 
+$end_num = get_num('Please give end number: ');
 
-fwrite(STDOUT, 'Please give end number: ');
-$end_num = trim(fgets(STDIN));
-
+// do {
+// 	fwrite(STDOUT, 'Please give end number: ');
+// 	$end_num = trim(fgets(STDIN));
+// 	if (!is_numeric($end_num)) {
+// 		echo "You must give a number. Please try again.\n";
+// 	}
+// } while (!is_numeric($end_num));
 
 fwrite(STDOUT, 'Please give the increment you would like to count by: ');
 $count_increment = trim(fgets(STDIN));
@@ -33,6 +48,17 @@ var_dump($count_increment);
 for ($i = $start_num; $i <= $end_num; $i += $count_increment) {
     echo "\$i has a value of {$i}\n";
 }
+
+
+
+
+// if (is_numeric($argv[1])) {
+// 	$min = intval($argv[1]);
+// } else {
+// 	$min = 1;
+// }
+// $max = is_numeric($argv[2]) ? intval($argv[2]) : 100;
+
 
 
 //$min = is_numeric($argv[1]) ? intval($argv([1]) : 1;
