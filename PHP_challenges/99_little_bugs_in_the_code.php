d14619b8984ffaa2035beb862cce1550a2c876c7
sharonranels <sharonranels@yahoo.com>
2014-03-15 15:30:01 -0500

    this version shows 0 bugs before saying have a beer - the last version had the final comment showing before the echo actually showed zero

--- PHP_challenges/99_little_bugs_in_the_code.php
+++ PHP_challenges/99_little_bugs_in_the_code.php
@@ -3,26 +3,28 @@
 
 
 $bugs = 99;
-$bugs2 = mt_rand(0, 117);
 
 do {
 	if ($bugs > 1) {
 		if ($bugs2 > 1) {
-			echo "$bugs little bugs in the code, $bugs little bugs.\n Take one out, patch it about, $bugs2 little bugs in the code\n";
+			echo "$bugs little bugs in the code, $bugs little bugs.\n Take one out, patch it about, $bugs2 little bugs in the code.\n";
 				} elseif ($bugs2 == 1) {
 				echo "$bugs little bugs in the code, $bugs little bugs.\n Take one out, patch it about, $bugs2 little bug in the code.\n";
-			}
+				} else {
+					echo "$bugs little bugs in the code, $bugs little bugs.\n Take one out, patch it about, $bugs2 little bugs in the code - GO HAVE A BEER!.\n";
+				}
 		} elseif ($bugs == 1) {
 			if ($bugs2 > 1) {
-			echo "$bugs little bug in the code, $bugs little bug.\n Take one out, patch it about, $bugs2 little bugs in the code\n";
+			echo "$bugs little bug in the code, $bugs little bug.\n Take one out, patch it about, $bugs2 little bugs in the code.\n";
 			} elseif ($bugs2 == 1) {
 				echo "$bugs little bugs in the code, $bugs little bugs.\n Take one out, patch it about, $bugs2 little bug in the code.\n";
-			} 
+			} else {
+				echo "$bugs little bugs in the code, $bugs little bugs.\n Take one out, patch it about, $bugs2 little bugs in the code - GO HAVE A BEER!.\n";
+			}
 		}
 
 	$bugs = $bugs2;
 	$bugs2 = mt_rand(0,117);
-} while ($bugs > 0);
-echo "DONE! Go have a beer!\n";
+} while (($bugs2 > 0) || ($bugs > 0));
 
 ?>
